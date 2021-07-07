04d71133a1f0e055c6c67745c0bc7bf064bb0a3e
Сорок два <sorokdva.developer@gmail.com>
2021-07-08 01:29:06 +0200

    feat(nasa-module): apod

--- src/connectors/nasa/apod.ts
+++ src/connectors/nasa/apod.ts
@@ -1,7 +1,7 @@
 import { env } from '@materya/carbon'
 import { httpClientHelper } from '../../helpers'
 
-export type NasaApodGetResponse = {
+export type OneRecordAnswer = {
   title: string
   copyright: string
   date: Date
@@ -12,14 +12,25 @@ export type NasaApodGetResponse = {
   url: string
 }
 
-const get = async (): Promise<NasaApodGetResponse> => {
+export type NasaApodGetResponse = OneRecordAnswer | Array<OneRecordAnswer>
+
+const get = async (opts?: {
+  date?: Date
+  count?: number
+}): Promise<NasaApodGetResponse> => {
+  let dateObj = new Date()
+
+  if (opts?.date) dateObj = new Date(opts.date)
+  const format = `${dateObj.getFullYear()}-${dateObj.getMonth()}-${dateObj.getDate()}`
   const endpoint = `${env.get('NASA_APOD_API_URL')}${env.get('NASA_API_KEY')}`
+  const dateQuery = `${opts?.date ? `&date=${format}}` : ''}`
+  const countQuery = `${opts?.count && !opts.date ? `&count=${opts.count}` : ''} `
+  const finalEndpoint = `${endpoint}${dateQuery ?? ''}${countQuery ?? ''}`
   try {
     return await httpClientHelper.get<NasaApodGetResponse>(
-      `${endpoint}`,
+      finalEndpoint,
     )
   } catch (error) {
-    console.error(error)
     throw new Error(error)
   }
 }
--- src/modules/nasa.ts
+++ src/modules/nasa.ts
@@ -1,46 +1,91 @@
-import { MessageEmbed } from 'discord.js'
+import { MessageEmbed, User } from 'discord.js'
 import { nasa } from '../connectors'
 import { translateHelper } from '../helpers'
+import { OneRecordAnswer } from '../connectors/nasa/apod'
 
-const apod = async (): Promise<MessageEmbed> => {
-  const data = await nasa.apod.get()
+const apod = async (opts?: {
+  date?: Date
+  count?: number
+  author?: User
+  notif?: boolean
+}): Promise<MessageEmbed | Array<MessageEmbed> | undefined> => {
+  const data = await nasa.apod.get(opts)
+  console.log(data)
+  const askedBy = opts?.author ? `・par ${opts.author.username}` : ''
 
-  const [title, desc] = ((await translateHelper
-    .translate([data.title, data.explanation], 'fr'))).translations
+  if (!opts?.count) {
+    const record = <OneRecordAnswer>data
+    const tr = (await translateHelper
+      .translate([record.title, record.explanation], 'fr'))
+      .translations
 
-  return new MessageEmbed()
-    .setColor('RANDOM')
-    .setTitle(title.text)
-    .setDescription(`<@&853036396205834261>, ${desc.text}`)
-    .setImage(data.hdurl)
-    .addField('Copyright', `_${data.copyright}_`, true)
-    .addField('Date:', data.date, true)
-    .setFooter('Récupérée via l\'api de la NASA et traduit avec Yandex')
-    .setTimestamp()
+    const [title, desc] = tr.map((t: { text: string }) => t.text)
+    const embed = new MessageEmbed()
+      .setColor('RANDOM')
+      .setTitle(title)
+      .setDescription(`${opts?.notif ? '<@&853036396205834261>, ' : ''}${desc}`)
+
+    if (record.media_type === 'video') {
+      embed.addField('<:YouTube:854824522069573662>・Vidéo', record.url)
+      embed.setURL(record.url)
+    } else {
+      embed.setImage(record.hdurl)
+    }
+
+    embed.addField('(c)', `_${record.copyright ?? 'Inconnu'}_`, true)
+      .addField('Date', `_${record.date}_`, true)
+      .setFooter(`Via l'api de la NASA・Traduit avec Yandex${askedBy}`)
+      .setTimestamp()
+
+    return embed
+  }
+
+  const records = <Array<OneRecordAnswer>> data
+  console.log(records)
+
+  const embeds: Array<MessageEmbed> = await Promise.all(
+    records.map(async record => {
+      const trans = (await translateHelper
+        .translate([record.title, record.explanation], 'fr'))
+        .translations
+      const [title, desc] = trans.map(t => t.text)
+
+      const embed = new MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle(title)
+        .setDescription(`${opts?.notif ? '<@&853036396205834261>, ' : ''}${desc}`)
+
+      if (record.media_type === 'video') {
+        embed.addField('<:YouTube:854824522069573662> vidéo', record.url)
+        embed.setURL(record.url)
+      } else {
+        embed.setImage(record.hdurl)
+      }
+
+      embed.addField('(c)', `_${record.copyright ?? 'Inconnu'}_`, true)
+        .addField('Date', `_${record.date}_`, true)
+        .setFooter(`Via l'api de la NASA et traduit avec Yandex. ${askedBy}`)
+        .setTimestamp()
+
+      return embed
+    }),
+  )
+
+  return embeds
 }
 
-const imageSearch = async (): Promise<MessageEmbed> => {
-  const data = await nasa.apod.get()
+const imageSearch = async (opts?: {
+  date?: Date
+  count?: number
+  author?: User
+  notif?: boolean
+}): Promise<MessageEmbed> => {
+  const data = <OneRecordAnswer> await nasa.apod.get()
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   const [title, desc] = ((await translateHelper
-    .translate([data.title, data.explanation], 'fr'))).translations
-
-  return new MessageEmbed()
-    .setColor('RANDOM')
-    .setTitle(title.text)
-    .setDescription(`<@&853036396205834261>, ${desc.text}`)
-    .setImage(data.hdurl)
-    .addField('Copyright', data.copyright, true)
-    .addField('Date:', data.date, true)
-    .setFooter('Récupérée via l\'api de la NASA et traduit avec Yandex')
-    .setTimestamp()
-}
-
-const apodSearchDate = async (): Promise<MessageEmbed> => {
-  const data = await nasa.apod.get()
-
-  const [title, desc] = ((await translateHelper
-    .translate([data.title, data.explanation], 'fr'))).translations
+    .translate([data.title, data.explanation], 'fr')))
 
   return new MessageEmbed()
     .setColor('RANDOM')
@@ -55,6 +100,5 @@ const apodSearchDate = async (): Promise<MessageEmbed> => {
 
 export {
   apod,
-  apodSearchDate,
   imageSearch,
 }
